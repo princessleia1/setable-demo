c201b79a7ae9d6ae7f05775c1e2b26697c8c6c43
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-10-14 19:14:48 +1000

    Add logging

--- companion/index.js
+++ companion/index.js
@@ -1,5 +1,12 @@
 import * as messaging from "messaging";
 import { settingsStorage } from "settings";
+import { me } from "companion";
+
+// Internet access 
+console.log("Application ID: " + me.applicationId);
+if (!me.permissions.granted("access_internet")) {
+   console.log("We're not allowed to access the internet :-(");
+}
 
 // Message socket opens
 messaging.peerSocket.onopen = () => {
